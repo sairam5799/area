e2fc0ab43616c050d96e1eb92ee82481ab6ab83b
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 11:56:27 +0530

    Completed Successfully

--- com.ramsai/src/com/ramsai/circle.java
+++ com.ramsai/src/com/ramsai/circle.java
@@ -2,6 +2,17 @@ package com.ramsai;
 
 public class circle {
 	
-	//code
+	import java.util.Scanner;
+
+	class Main {
+	  public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Enter the radius of the circle: ");
+	    double radius = input.nextDouble();
+	    double area = Math.PI * radius * radius;
+	    System.out.println("The area of the circle is " + area);
+	  }
+	}
+
 
 }
--- com.ramsai/src/com/ramsai/cuboid.java
+++ com.ramsai/src/com/ramsai/cuboid.java
@@ -1,5 +1,16 @@
 package com.ramsai;
 
 public class cuboid {
-//code
+	import java.util.Scanner;
+
+	class Main {
+	  public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Enter the radius of the circle: ");
+	    double radius = input.nextDouble();
+	    double area = Math.PI * radius * radius;
+	    System.out.println("The area of the circle is " + area);
+	  }
+	}
+
 }
